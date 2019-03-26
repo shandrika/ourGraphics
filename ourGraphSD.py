bcfe72205b6e7b726d6faa84e866b4aede50624e
csPython <cspython@AD25TF04NGF1J-78.local>
2019-03-26 16:10:15 -0400

    switched to rect function

--- ourGraphSD.py
+++ ourGraphSD.py
@@ -1,15 +1,15 @@
 from graphics import *
 
-def draw_sky(sX, sY, color, size, win):
-    square = Rectangle(Point(sX, sY),
-                       Point(sX + size, sY + size))
+def draw_rect(rX, rY, color, sizeX, sizeY, win):
+    square = Rectangle(Point(rX, rY),
+                       Point(rX + sizeX, rY + sizeY))
     square.setFill(color)
     square.draw(win)
 
 sqSz = 100
 
-win = GraphWin("Beach", sqSz * 10, sqSz * 10)
+win = GraphWin("Let's Play Volleyball!", sqSz * 10, sqSz * 10)
 win.setCoords(0,0, sqSz * 10, sqSz * 10)
 
-draw_sky(sqSz, sqSz, "sky blue", sqSz * 10, win)
+draw_rect(sqSz * 0, sqSz * 0, "sky blue", sqSz * 10, sqSz * 10, win)
 
