8cbb33ae0dbac995198361fffb1b0cd35141b0cc
csPython <cspython@AD25TF04NGF1J-78.local>
2019-03-26 16:29:42 -0400

    ball, wrong spot

--- ourGraphSD.py
+++ ourGraphSD.py
@@ -16,7 +16,11 @@ def draw_be(bX, bY, sizeX, sizeY, win):
 def draw_h20(wX, wY, sizeX, sizeY, win):
     draw_rect(wX, wY, "blue", sizeX, sizeY, win)
     
-
+def draw_cir(cX, cY, radius, cColor, win):
+    circle = Circle(Point(cX, cY), radius)
+    circle. setFill(cColor)
+    circle.draw(win)
+    
 sqSz = 100
 
 win = GraphWin("Let's Play Volleyball!", sqSz * 10, sqSz * 10)
@@ -25,5 +29,6 @@ win.setCoords(0,0, sqSz * 10, sqSz * 10)
 draw_sky(0,0, sqSz * 10, sqSz * 10, win)
 draw_h20(0,0, sqSz * 10, sqSz * 7, win)
 draw_be(0,0, sqSz * 10, sqSz * 5, win)
+draw_cir(sqSz/2, sqSz/4, 34, "white", win)
 
 
