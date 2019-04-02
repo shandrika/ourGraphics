6d88fac7f7d862f71708465819e2a021ab394b90
csPython <cspython@AD25TF04NGF1J-64.local>
2019-04-02 11:57:23 -0400

    Created Minerals For Sand

--- ourGraphSD.py
+++ ourGraphSD.py
@@ -1,4 +1,5 @@
 from graphics import *
+from random import*
 
 def draw_rect(rX, rY, rColor, sizeX, sizeY, win):
     square = Rectangle(Point(rX, rY),
@@ -11,7 +12,7 @@ def draw_sky(sX, sY, sizeX, sizeY, win):
     draw_rect(sX, sY, "sky blue", sizeX, sizeY, win)
     
 def draw_be(bX, bY, sizeX, sizeY, win):
-    draw_rect(bX, bY, "yellow", sizeX, sizeY, win)
+    draw_rect(bX, bY, "light yellow", sizeX, sizeY, win)
     
 def draw_h20(wX, wY, sizeX, sizeY, win):
     draw_rect(wX, wY, "blue", sizeX, sizeY, win)
@@ -31,4 +32,9 @@ draw_h20(0,0, winSz, winSz * .7, win)
 draw_be(0,0, winSz, winSz * .5, win)
 draw_cir(winSz/2, winSz/4, 34, "white", win)
 
-
+# Creation of Sand Spots
+for s in range(500):
+    saX = randint(10,1000)
+    saY = randint(10,490)
+    saSz = randint(1,3)
+    draw_cir(saX, saY, saSz, "peachpuff1", win)
